95329c4acb11849795fe917a6bf9541969d1c286
Alpár Etele Kövér <alpar.kover@iquestgroup.com>
2020-11-02 19:19:30 +0200

    Added the example code.

---
+++ NUnit1011TypeInOtherAssemblyBug/TheTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using TheOtherAssembly;
+
+namespace NUnit1011TypeInOtherAssemblyBug
+{
+	class TheTest
+	{
+		[TestCaseSource(typeof(TestCaseSources), nameof(TestCaseSources.TheNames))]
+		public void ThisIsFoundAsNUnit1011Violation(string name)
+		{
+
+		}
+	}
+}
